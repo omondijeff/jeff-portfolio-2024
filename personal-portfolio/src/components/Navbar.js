0583cfb38036d6638af206c83a58098153aaa602
Omondi Jeff <11554jeff@gmail.com>
2024-02-04 21:00:04 +0300

    Setup

---
+++ personal-portfolio/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Stories</li>
+        <li>Works</li>
+        <li>Contacts</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
